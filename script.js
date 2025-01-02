6483fdf26738f9202de33543a5541c31838beaea
Jenah <fonsecasantosjean@gmail.com>
2025-01-02 19:52:50 -0300

    fix/corrigido possibilidades de grupos com mesmo nome

--- script.js
+++ script.js
@@ -4,40 +4,50 @@ const elementNome = document.getElementById('nome')
 const elementTel = document.getElementById('telefone')
 let qntddContatos = 0
 let linhas = ''
+let opcoesSelect = `<option value="" hidden disabled selected></option><option value="Amigo" >Amigos</option><option value="Trabalho">Trabalho</option><option value="Familia">Familia</option><option value="Urgencia">Urgencia</option><option value="custom">Outro</option>`
+let grupos = ['Amigo', 'Trabalho', 'Familia', 'Urgencia']
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
     
     
-    if (!verificaCustomClass()){
+    if (verificaCustomClass()){} else {
         atualizaTabela(adicionaLinha())
         atualizaContatosSalvos()
     }
+
+    elementNome.value = ''
+    elementTel.value = ''
+    elementGrupo.selectedIndex = 0
+
 })
 
 
 function verificaCustomClass() {
-    const indiceSelecionado = elementGrupo.value;
+    const valorSelect = elementGrupo.value;
 
-    if (indiceSelecionado === 'custom') {
+    if (valorSelect === 'custom') {
         novoGrupo()
         return true
+    } else {
+        return false
     }
 }
 
 function novoGrupo() {
     const newGroupName = prompt('Como deseja que seja o nome do grupo que deseja criar')
-    let opcoesSelect = `<option value="" hidden disabled selected></option><option value="Amigo" >Amigos</option><option value="Trabalho">Trabalho</option><option value="Familia">Familia</option><option value="Urgencia">Urgencia</option><option value="custon">Outro</option>`
+    
+    if (grupos.includes(newGroupName)) {
+        alert('Essa classificação jé está cadastrada')
+    } else {
     const elementSelect = document.querySelector('select')
     
     opcoesSelect += `<option value"${newGroupName}">${newGroupName}</option>`
     elementSelect.innerHTML = opcoesSelect
-
-    elementNome.value = ''
-    elementTel.value = ''
-    elementGrupo.selectedIndex = 0
     alert(`Classificação "${newGroupName}" foi adicionada com sucesso`)
-}
+    grupos.push(newGroupName)
+}}
 
 function adicionaLinha(){
 
